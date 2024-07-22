c08149bab58996b926fc8629b0639b96e55d75d2
Muhammad Abdullah <159704717+M-AbdullahEjaz@users.noreply.github.com>
2024-07-22 21:00:09 +0500

    Add files via upload

---
+++ kbc.py
@@ -0,0 +1,7 @@
+print("kon bnega crore patti using python")
+print("who are you")
+print("1. Amitabh Bachan")
+print("2. Contestant")
+a = input("Enter your choice (1, 2): ")
+if a == "2":
+    print("1. Bachan sah")
